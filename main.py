8a8b69f403656c57c06202d27fe9bead5a8a141e
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2020-11-19 13:19:00 +0530

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request,redirect,session
+from flask import Flask,render_template,url_for,request,redirect,session,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
